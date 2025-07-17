d66370ca09d4280157a4be358144bcbf2c92b07a
Smera820 <smerasabu@gmail.com>
2025-07-17 16:37:10 +0530

    Update index.js

--- Mern_blogs_app-backend/index.js
+++ Mern_blogs_app-backend/index.js
@@ -13,7 +13,7 @@ const commentRoute = require('./routes/comments')
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://fullstack-blog-app-frontend-2o48.onrender.com',
     credentials: true,
 };
 app.use(cors(corsOptions))
