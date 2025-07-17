5f637cb42806348d6da9de927b9bb955cdd9f156
Smera820 <smerasabu@gmail.com>
2025-07-17 16:34:17 +0530

    Update url.js

--- Mern_blogs_app-frontend/src/url.js
+++ Mern_blogs_app-frontend/src/url.js
@@ -1,2 +1,2 @@
 export const URL = "https://fullstack-blog-app-backend-icv2.onrender.com";
-export const IF="http://localhost:8000/images/"
+export const IF="https://fullstack-blog-app-backend-icv2.onrender.com/images/"
